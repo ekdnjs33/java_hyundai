88b719183652d521b40d822caf0cee5ec16c7779
다원 <gwi02029@gmail.com>
2021-01-15 17:27:00 +0900

    update

--- Day05/src/com/app1_자바IO/IOTest.java
+++ Day05/src/com/app1_자바IO/IOTest.java
@@ -1,47 +1,34 @@
 package com.app1_자바IO;
 
-/*
- * 
- *     자바 I/O (Input/Output)
- *     
- *     1. 용어 정리
- *     
- *     from                        to
- *     키보드(표준입력)    | 자바 |     모니터(표준출력) : System.out.println(값);
- *     파일                         파일
- *     네트워크                      네트워크
- *     
- *     입출력 가능한 기본데이터
- *     - byte
- *     		: InputStream
- *     				- FileInputStream
- *     				  ...
- *     				- ObjectInputStream
- *     
- *     		: OutputStream
- *     				- FileOutputStream
- *     				  ...
- *     				- ObjectInputStream
- *     - char
- *     		: Reader
- *     				- InputStreamReader
- *     				- BufferedReader
- *     				- FileReader
- *     				...
- *     
- *     		: Writer
- *     				- PrintWriter
- *     				- FileWriter
- *     				...
- *     
- */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class IOTest {
 
 	public static void main(String[] args) {
 
-		// 표준출력 ==> 콘솔에 출력
-		System.out.println("hello");
+		InputStream is = System.in; // byte처리, 한 글자 밖에 못 읽어
+		InputStreamReader reader = null; //byte --> char로 읽도록 변환
+		BufferedReader buffer = null; // char --> 한 라인씩 문자열로 읽도록 변환
+		System.out.println("이름입력");
+		
+		reader = new InputStreamReader(is);
+		buffer = new BufferedReader(reader);
+		
+		try {
+			String str = buffer.readLine();
+			System.out.println(str);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(buffer!=null)
+					buffer.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
-
 }
